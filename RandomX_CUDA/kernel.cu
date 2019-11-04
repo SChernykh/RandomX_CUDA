f441b505665b6f9aa4eec99d9ff191e61af65b12
SChernykh <sergey.v.chernykh@gmail.com>
2019-11-04 14:31:26 +0100

    Update kernel.cu

--- RandomX_CUDA/kernel.cu
+++ RandomX_CUDA/kernel.cu
@@ -65,7 +65,7 @@ int main(int argc, char** argv)
 
 	uint32_t flags;
 	if (cudaGetDeviceFlags(&flags) == cudaSuccess)
-		cudaSetDeviceFlags(flags | cudaDeviceScheduleBlockingSync);
+		cudaSetDeviceFlags(flags | cudaDeviceScheduleBlockingSync | cudaDeviceMapHost);
 
 	bool validate = false;
 	int bfactor = 0;
